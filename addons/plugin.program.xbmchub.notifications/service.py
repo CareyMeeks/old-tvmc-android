a2ea8d88ae4fa7afd1eb53e4062290d460077831
Quick Fox <quickfoxlazydog96+git@gmail.com>
2014-10-12 00:39:10 -0400

    Revert to base set of addons -- changing approach due to play store file size constraints

--- addons/plugin.program.xbmchub.notifications/service.py
+++
@@ -1,9 +0,0 @@
-import xbmc,string,logging,array
-import common as Common #from common import * #import common
-## ################################################## ##
-## ################################################## ##
-## Start of program
-TypeOfMessage="t"; (NewImage,NewMessage)=Common.FetchNews(); 
-Common.CheckNews(TypeOfMessage,NewImage,NewMessage,True); 
-## ################################################## ##
-## ################################################## ##
